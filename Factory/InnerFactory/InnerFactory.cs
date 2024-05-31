e10bbdf3acc6a78f4557dbd0aec83b77dc2850ba
Jack Heywood <jackaheywood@gmail.com>
2024-05-31 22:07:41 +0100

    Inner factory

---
+++ Factory/InnerFactory/InnerFactory.cs
@@ -0,0 +1,18 @@
+﻿using Utilities;
+using static System.Console;
+
+namespace Factory.InnerFactory;
+
+public class InnerFactory() : ConsoleProgram("Inner Factory")
+{
+    public override Task RunAsync()
+    {
+        var polarPoint = Point.Factory.NewPolarPoint(1.7, Math.PI);
+        WriteLine($"Polar:\t\t{polarPoint}");
+
+        var cartesianPoint = Point.Factory.NewCartesianPoint(-0.1, 7.2);
+        WriteLine($"Cartesian:\t{cartesianPoint}");
+
+        return Task.CompletedTask;
+    }
+}
--- Factory/Program.cs
+++ Factory/Program.cs
@@ -2,6 +2,7 @@
 using Factory.BulkReplacement;
 using Factory.FactoryClass;
 using Factory.FactoryMethod;
+using Factory.InnerFactory;
 using Factory.ObjectTracking;
 using Utilities;
 
@@ -11,6 +12,7 @@ var runner = new ProgramRunnerBuilder()
     .WithProgram(ConsoleKey.D3, new FactoryClass())
     .WithProgram(ConsoleKey.D4, new ObjectTracking())
     .WithProgram(ConsoleKey.D5, new BulkReplacement())
+    .WithProgram(ConsoleKey.D6, new InnerFactory())
     .Build();
 
 await runner.RunAsync();
