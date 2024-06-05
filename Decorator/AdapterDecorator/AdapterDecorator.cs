2ece9972c7be259f8457ca1fdcac0927020ffa35
Jack Heywood <jack.heywood@tisegroup.com>
2024-06-05 16:39:18 +0100

    Adapter-decorator

---
+++ Decorator/AdapterDecorator/AdapterDecorator.cs
@@ -0,0 +1,17 @@
+﻿using Utilities;
+using static System.Console;
+
+namespace Decorator.AdapterDecorator;
+
+public class AdapterDecorator() : ConsoleProgram("Adapter-Decorator")
+{
+    public override Task RunAsync()
+    {
+        // Simple refactoring to solve immutable string problem!
+        MyStringBuilder s = "Hello";
+        s += " World!";
+        WriteLine(s);
+
+        return Task.CompletedTask;
+    }
+}
--- Decorator/Program.cs
+++ Decorator/Program.cs
@@ -1,8 +1,10 @@
-﻿using Decorator.CustomStringBuilder;
+﻿using Decorator.AdapterDecorator;
+using Decorator.CustomStringBuilder;
 using Utilities;
 
 var runner = new ProgramRunnerBuilder()
     .WithProgram(new CustomStringBuilder())
+    .WithProgram(new AdapterDecorator())
     .Build();
 
 await runner.RunAsync();
