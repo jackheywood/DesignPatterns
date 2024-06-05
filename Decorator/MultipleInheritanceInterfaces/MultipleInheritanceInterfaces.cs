a244d91bbf8cc6f64b2664bc7d5a2bd667110a82
Jack Heywood <jack.heywood@tisegroup.com>
2024-06-05 17:03:37 +0100

    Multiple inheritance with interfaces

---
+++ Decorator/MultipleInheritanceInterfaces/MultipleInheritanceInterfaces.cs
@@ -0,0 +1,21 @@
+﻿using Utilities;
+
+namespace Decorator.MultipleInheritanceInterfaces;
+
+public class MultipleInheritance() : ConsoleProgram("Multiple Inheritance with Interfaces")
+{
+    public override Task RunAsync()
+    {
+        var bird = new Bird { Weight = 15 };
+        var lizard = new Lizard { Weight = 7 };
+        var dragon = new Dragon { Weight = 375 };
+
+        List<IBird> birds = [bird, dragon];
+        List<ILizard> lizards = [lizard, dragon];
+
+        foreach (var b in birds) b.Fly();
+        foreach (var l in lizards) l.Crawl();
+
+        return Task.CompletedTask;
+    }
+}
--- Decorator/Program.cs
+++ Decorator/Program.cs
@@ -1,10 +1,12 @@
 ﻿using Decorator.AdapterDecorator;
 using Decorator.CustomStringBuilder;
+using Decorator.MultipleInheritanceInterfaces;
 using Utilities;
 
 var runner = new ProgramRunnerBuilder()
     .WithProgram(new CustomStringBuilder())
     .WithProgram(new AdapterDecorator())
+    .WithProgram(new MultipleInheritance())
     .Build();
 
 await runner.RunAsync();
