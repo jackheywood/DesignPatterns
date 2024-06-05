8a6296dc12d588b30c4ce5176f0718f3dcdd5f38
Jack Heywood <jack.heywood@tisegroup.com>
2024-06-05 16:30:25 +0100

    Custom string builder

---
+++ Decorator/CustomStringBuilder/CustomStringBuilder.cs
@@ -0,0 +1,19 @@
+﻿using Utilities;
+using static System.Console;
+
+namespace Decorator.CustomStringBuilder;
+
+public class CustomStringBuilder() : ConsoleProgram("Custom String Builder")
+{
+    public override Task RunAsync()
+    {
+        var cb = new CodeBuilder();
+        cb.AppendLine("class Foo")
+            .AppendLine("{")
+            .AppendLine("}");
+
+        WriteLine(cb);
+
+        return Task.CompletedTask;
+    }
+}
---
+++ Decorator/Program.cs
@@ -0,0 +1,8 @@
+﻿using Decorator.CustomStringBuilder;
+using Utilities;
+
+var runner = new ProgramRunnerBuilder()
+    .WithProgram(new CustomStringBuilder())
+    .Build();
+
+await runner.RunAsync();
