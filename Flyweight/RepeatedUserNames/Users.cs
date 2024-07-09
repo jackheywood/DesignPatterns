a26f96e02f7f1fed08197dfee003a1cdd6e1bc44
Jack Heywood <jack.heywood@tisegroup.com>
2024-07-09 16:41:13 +0100

    Repeated user names

---
+++ Flyweight/RepeatedUserNames/Users.cs
@@ -0,0 +1,28 @@
+﻿namespace Flyweight.RepeatedUserNames;
+
+public class User(string fullName)
+{
+    public string FullName { get; } = fullName;
+}
+
+public class User2
+{
+    private static readonly List<string> Strings = [];
+    private readonly int[] _names;
+
+    public User2(string fullName)
+    {
+        _names = fullName.Split(' ').Select(GetOrAdd).ToArray();
+        return;
+
+        int GetOrAdd(string s)
+        {
+            var i = Strings.IndexOf(s);
+            if (i != -1) return i;
+            Strings.Add(s);
+            return Strings.Count - 1;
+        }
+    }
+
+    public string FullName => string.Join(' ', _names.Select(i => Strings[i]));
+}
