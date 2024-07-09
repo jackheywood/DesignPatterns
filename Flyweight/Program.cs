46c9837a73b58af577e36495ae6cfe6c41e26d47
Jack Heywood <jack.heywood@tisegroup.com>
2024-07-09 16:58:01 +0100

    Text formatting

--- Flyweight/Program.cs
+++ Flyweight/Program.cs
@@ -1,6 +1,8 @@
-﻿using Utilities;
+﻿using Flyweight.TextFormatting;
+using Utilities;
 
 var runner = new ProgramRunnerBuilder()
+    .WithProgram(new TextFormatting())
     .Build();
 
 await runner.RunAsync();
