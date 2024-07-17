426b8ced21b348f4dcd1bdaa0b834fb24e38e23c
Jack Heywood <jack.heywood@tisegroup.com>
2024-07-17 14:06:17 +0100

    Property proxy

--- Proxy/Program.cs
+++ Proxy/Program.cs
@@ -1,8 +1,10 @@
-﻿using Proxy.ProtectionProxy;
+﻿using Proxy.PropertyProxy;
+using Proxy.ProtectionProxy;
 using Utilities;
 
 var runner = new ProgramRunnerBuilder()
     .WithProgram(new ProtectionProxy())
+    .WithProgram(new PropertyProxy())
     .Build();
 
 await runner.RunAsync();
